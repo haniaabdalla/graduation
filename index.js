8b34f02693593a36d85469b3a3980fe313ee4bb9
Hania Abdalla <64498492+haniaabdalla@users.noreply.github.com>
2022-07-20 16:22:41 +0200

    Update index.js

--- index.js
+++ index.js
@@ -52,6 +52,4 @@ mongoose
         console.log("Database Connected");
     });
 
-app.listen(5000, () => {
-    console.log("Server is running on port 5000!");
-})
+app.listen(process.env.PORT || 5000)
